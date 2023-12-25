0cbdc3755397e4102522f1836df5104c7c7f406e
Sushant Rode <sushant.rode@gmail.com>
2023-12-25 16:23:18 +0000

    web ui

---
+++ UI/webui.py
@@ -0,0 +1,7 @@
+from pywebio.input import *
+
+input("This is first Web UI programme try")
+select("This is a drop down menu", ["Option1", "Option2", "Option 3 "])
+checkbox("Multiple Choices!", options=["a", "b", "c", "d"])
+radio("Select any one", options=["1", "2", "3"])
+textarea("Text Area", rows=2, placeholder="Write 2 lines about shweta")
